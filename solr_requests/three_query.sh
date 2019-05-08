1679a5dce1a5447e428ab4523b51d06c692cd3a5
uncoyote <jeker.git@outlook.de>
2019-05-08 08:19:07 +0200

    added fq on superclass

--- solr_requests/three_query.sh
+++ solr_requests/three_query.sh
@@ -4,13 +4,17 @@
 # Prioritäten der Suchfelder (search_1 > search_2 > search_3) 
 # sowie der exakten Treffer versus Wildcard-Treffer mittels boosting.
 # 
+# Verstehe nur so halbwegs, wieso es ohne zweites fq auf search_1 - _3
+# deutlich besser performt...
+# Zweites fq: "fq=(search_3:*$1* AND search_3:*$2* AND search_3:*$3*)" \
+# 
 # date +%s%3N gibt die Systemzeit in Millisekunden aus
 
 start=`date +%s%3N`
 
 curl -v -G \
 --data-urlencode "omitHeader=true" \
---data-urlencode "fq=(search_3:*$1* AND search_3:*$2* AND search_3:*$3*)" \
+--data-urlencode "fq=(superclass:dataproduct OR superclass:ch.so.agi.av.grundstuecke.rechtskraeftig OR superclass:ch.so.agi.av.gebaeudeadressen.gebaeudeeingaenge)" \
 --data-urlencode "q=( (search_1:$1^6 OR search_1:*$1*^5) AND (search_1:$2^6 OR search_1:*$2*^5) AND (search_1:$3^6 OR search_1:*$3*^5) )" \
 --data-urlencode "OR ( (search_2:$1^4 OR search_2:*$2*^3) AND (search_2:$2^4 OR search_2:*$2*^3) AND (search_2:$3^4 OR search_2:*$3*^3) )" \
 --data-urlencode "OR ( (search_3:$1^2 OR search_3:*$2*^1) AND (search_3:$2^2 OR search_3:*$2*^1) AND (search_3:$3^2 OR search_3:*$3*^1) )" \
@@ -23,6 +27,16 @@ end=`date +%s%3N`
 millis=`expr $end - $start`
 
 echo "Query took Solr $millis milliseconds to process"
+
+
+
+
+
+
+
+
+
+
 
 
 
