54335335d8a00f70ecb71abd1c8ef8fdd54c126b
bjsvwpfe <michael.pfeiffer@bd.so.ch>
2019-08-27 12:22:22 +0200

    Added list of ddl scripts for all productive views

---
+++ sourcedb/ddl_script/prod_views.sql
@@ -0,0 +1,16 @@
+psql -d pub -h geodb.rootso.org -f ada_adagisa_pub/v2_ada_adagisa_pub.fundstellen_solr_v.sql
+psql -d pub -h geodb.rootso.org -f afu_abbaustellen_pub/v3_afu_abbaustellen_pub.abbaustelle_solr_v.sql
+psql -d pub -h geodb.rootso.org -f afu_altlasten_pub/v3_afu_altlasten_pub.belastete_standorte_solr_v.sql
+psql -d pub -h geodb.rootso.org -f afu_ingeso_pub/v2_afu_ingeso_pub.geotope_solr_v.sql
+psql -d pub -h geodb.rootso.org -f afu_erdwaerme_pub/v3_afu_erdwaerme_pub.gew_bew_ad_solr_v.sql
+psql -d pub -h geodb.rootso.org -f agi_mopublic_pub/v3_agi_mopublic_pub_solr_v.sql
+psql -d pub -h geodb.rootso.org -f agi_hoheitsgrenzen_pub/v3_agi_hoheitsgrenzen_pub_solr_v.sql
+psql -d pub -h geodb.rootso.org -f alw_gelan_pub/v2_alw_gelan_pub_solr_v.sql
+psql -d pub -h geodb.rootso.org -f arp_naturreservate_pub/v2_arp_naturreservate_pub_solr_v.sql
+psql -d pub -h geodb.rootso.org -f avt_kantonsstrassen_pub/v5_avt_kantonsstrassen_pub_solr_v.sql
+psql -d pub -h geodb.rootso.org -f avt_oeffentlicher_verkehr_pub/v2_avt_oeffentlicher_verkehr_pub_solr_v.sql
+psql -d pub -h geodb.rootso.org -f avt_verkehrszaehlstellen_pub/v2_avt_verkehrszaehlstellen_pub_solr_v.sql
+psql -d pub -h geodb.rootso.org -f awjf_wald_pub/v9_awjf_wald_pub_solr_v.sql
+psql -d pub -h geodb.rootso.org -f afu_gewisso_pub/v4_afu_gewisso_pub.jfv_fischenz_solr_v.sql
+psql -d pub -h geodb.rootso.org -f awjf_jagd_pub/v3_awjf_jagd_pub_solr_v.sql
+
